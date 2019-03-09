b7876f27fa0061e5dbc2dd20f382d04154af3643
Shubham Singal <singalshubham312@gmail.com>
2019-03-10 01:16:14 +0530

    copied the structure

--- Frontend/app/actions/fetchTweets.js
+++
@@ -1,8 +0,0 @@
-const fetchTweets=async()=>{
-    try{
-        const res=await fetch('http://localhost:5000/api/home')
-        return res.json().tweets
-    }catch(err){
-        console.log(err)
-    }
-}
--- Frontend/app/index.jsx
+++ Frontend/app/index.jsx
@@ -1,21 +1,10 @@
-// initialize react here
 import React from 'react';
 import { render } from 'react-dom';
-import { } from 'reactstrap';
 
 import './styles/main.scss';
 
 class App extends React.Component {
-
   render() {
-
-    return(
-      <div>
-        <h1>Login</h1>
-        <input type="text" placeholder="username"/>
-        <input type="text" placeholder="password"/>
-      </div>
-    )
     
   }
 }
